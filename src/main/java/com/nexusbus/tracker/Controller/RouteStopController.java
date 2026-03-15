f55232389bfaaae11fa8f51f5045d8d618109c15
NavtejSutar <navtejsutar@gmail.com>
2026-03-15 09:42:26 +0530

    more user and routeStop endpoints

--- src/main/java/com/nexusbus/tracker/Controller/RouteStopController.java
+++ src/main/java/com/nexusbus/tracker/Controller/RouteStopController.java
@@ -4,12 +4,14 @@ import java.util.*;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.nexusbus.tracker.Entities.BusStop;
 import com.nexusbus.tracker.Entities.Route;
@@ -74,6 +76,14 @@ public class RouteStopController {
         }catch(Exception e) {
             return ResponseEntity.internalServerError().build();
         }        
+    }
+
+    @DeleteMapping("/admin/routeStop/{routeStopId}")
+    public ResponseEntity<?> deleteRouteStop(@PathVariable int routeStopId){
+        RouteStop routeStop=routeStopRepo.findById(routeStopId).orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "RouteStop not found"));;
+        routeStopRepo.delete(routeStop);
+        return ResponseEntity.ok("RouteStop Deleted Succesfully");
     }
     
 }
--- src/main/java/com/nexusbus/tracker/Controller/UsersController.java
+++ src/main/java/com/nexusbus/tracker/Controller/UsersController.java
@@ -5,15 +5,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.nexusbus.tracker.DTO.UsersDto;
 import com.nexusbus.tracker.Entities.Users;
 import com.nexusbus.tracker.Repository.UsersRepo;
 
+import java.util.*;
 
 @RestController
 public class UsersController {
@@ -59,5 +65,46 @@ public class UsersController {
 
         return ResponseEntity.ok(userDetail);
     }
+
+    @PutMapping("/admin/users/{userId}/role")
+    public ResponseEntity<?> changeUsersRole(@PathVariable int userId, @RequestParam String role){
+        Users updatedUser=usersRepo.findById(userId).orElseThrow(() -> new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "User not found"));
+        updatedUser.setRole(role);
+        usersRepo.save(updatedUser);
+        UsersDto userDetail=new UsersDto(
+            updatedUser.getFName(),
+            updatedUser.getLName(),
+            updatedUser.getEmailId(),
+            updatedUser.getRole()
+        );
+        return ResponseEntity.ok(userDetail);
+    }
     
+    @GetMapping("/admin/users")
+    public ResponseEntity<?> getAllUsers() {
+
+        List<UsersDto> users = usersRepo.findAll()
+                .stream()
+                .map(user -> new UsersDto(
+                        user.getFName(),
+                        user.getLName(),
+                        user.getEmailId(),
+                        user.getRole()
+                ))
+                .toList();
+
+        return ResponseEntity.ok(users);
+    }
+    @DeleteMapping("/admin/users/{userId}")
+    public ResponseEntity<?> deleteUser(@PathVariable int userId) {
+
+        Users user = usersRepo.findById(userId)
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "User not found"));
+
+        usersRepo.delete(user);
+
+        return ResponseEntity.ok("User deleted successfully");
+    }
 }
